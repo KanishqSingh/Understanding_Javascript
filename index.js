65dff48aa7b7bb3856e9cf0cceeb4c4994676da3
Kanishq SIngh <singhkanishq06@gmail.com>
2025-02-20 20:42:49 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
 //output
-console.log([] + {} === {} + []) //true
+// console.log([] + {} === {} + []) //true
 
-let x = 1;
-let y = "1";
+// let x = 1;
+// let y = "1";
 
-console.log(++x, ++y); // 2 2
+// console.log(++x, ++y); // 2 2
 
 
-let a = true + true;
-let b = a + false;
+// let a = true + true;
+// let b = a + false;
 
-console.log(b); //2
+// console.log(b); //2
+
+// function test() {
+//     if (true) {
+//       let  = 10;
+//       var b = 20;
+//     }
+//     console.log(b);
+//     console.log(a);
+//   }
+//   test(); // true 20
+
+// const a = { b: { c: 2 } };
+// const b = { ...a };
+// a.b.c = 3;
+
+// console.log(b.b.c); //true 3
+
+// let x = [1, 2, 3];
+// let [, , y] = x;
+
+// console.log(y); //3
+
+// console.log("Start");
+// setTimeout(() => console.log("Inside Timeout"), 0);
+// console.log("End"); //start end inside timeout
+
+// const obj1 = { name: "Alice" };
+// const obj2 = obj1;
+// obj2.name = "Bob";
+// console.log(obj1.name); //bob
+
+// (function() {
+//     var a = 10;
+//     console.log(a);
+//   })();
+//   console.log(a); //10
+  
+
+
+
+
+  
