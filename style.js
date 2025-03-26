a19406c6a9d723fa32d129d10085fb5d71ce0d1d
Kanishq SIngh <singhkanishq06@gmail.com>
2025-03-26 19:36:54 +0530

    initial

--- style.js
+++ style.js
@@ -33,3 +33,30 @@
 //   console.log([counter2(), counter2(), counter2(), counter2(), counter2()]); // Output: [-2, -1, 0, 1, 2]
   
   
+class ArrayWrapper {
+    constructor(nums) {
+        this.nums = nums;
+    }
+
+    valueOf() {
+        return this.nums.reduce((sum, num) => sum + num, 0);
+    }
+
+    toString() {
+        return `[${this.nums.join(",")}]`;
+    }
+}
+
+// Example 1:
+const obj1 = new ArrayWrapper([1, 2]);
+const obj2 = new ArrayWrapper([3, 4]);
+console.log(obj1 + obj2); // Output: 10
+
+// Example 2:
+const obj3 = new ArrayWrapper([23, 98, 42, 70]);
+console.log(String(obj3)); // Output: "[23,98,42,70]"
+
+// Example 3:
+const obj4 = new ArrayWrapper([]);
+const obj5 = new ArrayWrapper([]);
+console.log(obj4 + obj5); // Output: 0
