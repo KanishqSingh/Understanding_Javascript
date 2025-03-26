a3a7c339e0c5ac76fd7dfec0c23391ce472a3c54
Kanishq SIngh <singhkanishq06@gmail.com>
2025-03-26 19:36:19 +0530

    initial

--- script.js
+++ script.js
@@ -143,3 +143,33 @@ sub1.unsubscribe();
 
 // Emit the event again
 console.log(emitter.emit("firstEvent", [5])); // Output: [7]
+
+Array.prototype.groupBy = function(fn) {
+    return this.reduce((acc, item) => {
+        const key = fn(item);
+        if (!acc[key]) {
+            acc[key] = [];
+        }
+        acc[key].push(item);
+        return acc;
+    }, {});
+};
+
+// Example 1:
+const array1 = [{"id":"1"}, {"id":"1"}, {"id":"2"}];
+console.log(array1.groupBy(item => item.id));
+// Output: { "1": [{"id": "1"}, {"id": "1"}], "2": [{"id": "2"}] }
+
+// Example 2:
+const array2 = [
+  [1, 2, 3],
+  [1, 3, 5],
+  [1, 5, 9]
+];
+console.log(array2.groupBy(list => String(list[0])));
+// Output: { "1": [[1, 2, 3], [1, 3, 5], [1, 5, 9]] }
+
+// Example 3:
+const array3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(array3.groupBy(n => String(n > 5)));
+// Output: { "true": [6, 7, 8, 9, 10], "false": [1, 2, 3, 4, 5] }
